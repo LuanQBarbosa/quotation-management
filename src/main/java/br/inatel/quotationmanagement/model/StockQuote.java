705892cad4618610dec35bd11a676900b79f7e0d
Luan Barbosa <luanqbarbosa@gmail.com>
2021-08-10 05:59:23 -0300

    First commit

---
+++ src/main/java/br/inatel/quotationmanagement/model/StockQuote.java
@@ -0,0 +1,12 @@
+package br.inatel.quotationmanagement.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class StockQuote {
+	
+	private Long id;
+	private BigDecimal value;
+	private LocalDate date;
+
+}
